264317c65221a7616a72278d43d7574904fdf379
Michał Ziółkowski <michal.ziolkowski@atos.net>
2024-03-17 19:36:01 +0100

    corrected typos, wrong loop logic and added input_pullup const to control the signal

--- CameraModule.ino
+++ CameraModule.ino
@@ -2,17 +2,19 @@
 
 Servo servo;
 
-SERVO_PIN = D7
-REVERSE_SIGNAL = D6
+const SERVO_PIN = D7
+const REVERSE_SIGNAL = D6
 
 void setup() {
   servo.attach(SERVO_PIN);
-  pinMode(REVERSE_SIGNAL, INPUT);
+  pinMode(REVERSE_SIGNAL, INPUT_PULLUP);
+}
 
-  while(digitalRead(REVERSE_SIGNAL) == HIGH) {
-    servo.write(180);
-
-    //Hold the servo in the reverse position for 10 seconds
-    sleep(10000);
-  }
+void loop() {
+    
+    if(digitalRead(REVERSE_SIGNAL) == HIGH) {
+        servo.write(180);
+        //Hold the servo in the reverse position for 10 seconds
+        sleep(10000);
+    }
 }
